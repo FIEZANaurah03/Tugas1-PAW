f0c586769d4249de73bfb86ad5122834a18730c9
Fieza Naurah Aprilia <106217900+FIEZANaurah03@users.noreply.github.com>
2022-08-29 18:58:39 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>NAURAH-TEKNIK INFORMATIKA-c</title>
+</head>
+
+<body>
+
+    <body style="background-color:plum"> </body>
+    <h1 align="center">BIODATA</h1>
+    <tr>
+        <center>
+            <td rowspan="7"><img src="na.jpg" width="300"></td>
+        </center>
+    </tr>
+    <table width="800" border="0" align="center" cellpadding="5">
+        <tr>
+            <td colspan="2">Basic Info
+                <hr />
+            </td>
+        </tr>
+        <tr>
+            <td width="50%" align="right">Full Name:</td>
+            <td>FIEZA NAURAH APRILIA</td>
+        </tr>
+        <tr>
+            <td align="right">KELAS:</td>
+            <td>Pemrograman Aplikasi Web (C)</td>
+        </tr>
+        <tr>
+            <td align="right">Jurusan</td>
+            <td><i> Teknik Informatika </td>
+        </tr>
+        <tr>
+            <td align="right">NIM:</td>
+            <td>210411100049</td>
+        </tr>
+        <tr>
+            <td align="right">Date of Birth:</td>
+            <td>21<sup>th</sup> April</td>
+        </tr>
+
+        <tr>
+            <td colspan="2">Contact Details
+                <hr />
+            </td>
+        </tr>
+        <tr>
+            <td align="right" valign="top">Address:</td>
+            <td>Jl.Raya Lenteng Dsn.Torbang Timur RT/RW: 01/02, Batuan, Sumenep</td>
+        </tr>
+        <tr>
+            <td align="right" valign="top">Phone Number:</td>
+            <td>087765311712</td>
+        </tr>
+        <tr>
+            <td align="right" valign="top">Email Address:</td>
+            <td>naurah.aprilia21@gmail.com</td>
+        </tr>
+        <tr>
+            <td colspan="2">Education
+                <hr>
+            </td>
+        </tr>
+        <tr>
+            <td align="right">Education:</td>
+            <td>Universitas Trunojoyo Madura</td>
+        </tr>
+    </table>
+    <script src="tugas1.js"></script>
+</body>
+
+</html>
